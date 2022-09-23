a366a16d6e0da47a850f72246e672eabf4baa48e
Conrad Russo <conradr@gmail.com>
2022-09-23 16:36:59 +0100

    Added contest seeds and contest api restful route

--- server/server.js
+++ server/server.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const cors = require('cors');
+const cors = require('cors')
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
+const MongoClient = require('mongodb').MongoClient
+const createRouter = require('./helpers/create_router.js')
 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
-    const db = client.db('eurovision');
-    const songsCollection = db.collection('songs');
-    const songsRouter = createRouter(songsCollection);
-    app.use('/api/all', songsRouter);
+    const db = client.db('eurovision')
+    const songsCollection = db.collection('songs')
+    const songsRouter = createRouter(songsCollection)
+    app.use('/api/all', songsRouter)
+    const contestCollection = db.collection('contests')
+    const contestRouter = createRouter(contestCollection)
+    app.use('/api/contests', contestRouter)
   })
-  .catch(console.err);
+  .catch(console.err)
 
 app.listen(9000, function () {
-  console.log(`Listening on port ${ this.address().port }`);
-});
+  console.log(`Listening on port ${this.address().port}`)
+})
