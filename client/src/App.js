fb307e7eab23ae50b7aa9fe236fcc093e07e2670
Steven Todd <steven@flare-photography.com>
2022-09-23 14:33:24 +0100

    update after merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import './App.css';
 import { getAll } from './services/EuroServices';
-import SongList from  './components/SongList';
+import SongsList from './components/SongList';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './containers/Home'
