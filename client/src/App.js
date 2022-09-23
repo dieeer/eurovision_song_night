920a444f36a36a7ad041db889073ad5a6b6ded9c
justin reid <hi@justinre.id>
2022-09-23 11:03:32 +0100

    added routes and base containers, working to conrad's wireframes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <NavBar/>
+      <Router>
+        <Routes>
+          <Route path="/"/>
+          <Route path="/songs" element={<SongsContainer />} />
+          <Route path="/playlists" element={<PlaylistsContainer />} />
+          <Route path="/contests" element={<ContestsContainer/>}/>
+          <Route path ="/*" element={<Error/>}/>
+        </Routes>
+      </Router>
+    </>
   );
-}
+  }
 
 export default App;
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <ul>
+            <li>
+                <Link to='/'>home</Link>
+            </li>
+            <li>
+                <Link to='/songs'>songs</Link>
+            </li>
+            <li>
+                <Link to='/playlists'>playlists</Link>
+            </li>
+            <li>
+                <Link to='/contests'>contests</Link>
+            </li>
+        </ul>
+
+    )
+}
+
+export default NavBar;
