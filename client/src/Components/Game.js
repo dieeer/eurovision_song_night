a2d5dbfa0f96840d2927fc82bdfb7a43bc2eb316
Conrad Russo <conradr@gmail.com>
2022-09-25 16:03:22 +0100

    update

--- client/src/Components/Game.js
+++ client/src/Components/Game.js
@@ -2,18 +2,48 @@ import { useState } from 'react'
 import YoutubeEmbed from './YoutubeEmbed'
 
 const Game = ({ playedGame }) => {
-const startSong = playedGame.playlist[0]['youtube_url']
-const [currentSong, setCurrentSong] = useState(startSong)
+  const startSong = playedGame.playlist[0]['youtube_url']
+  const [currentSong, setCurrentSong] = useState(startSong)
+   const [counterState, setCounterState] = useState(0)
+  const [button, setButton] = useState(true)
 
-const nextSong = () => {
-  setCurrentSong(playedGame.playlist[1]['youtube_url'])
-}
+  //const playListArray = playedGame.playlist
+  const playListArray = [0, 1, 2]
+  const playListLength = playListArray.length
+  //let counter = 0
 
+  const nextSong = () => {
+    if (counterState < playListLength) {
+      //counterState += 1
+      setCounterState(counterState + 1)
+      setCurrentSong(playedGame.playlist[counterState]['youtube_url'])
+    } else {
+      setButton(false)
+    }
+  }
+
+  const prevSong = () => {
+    if (counterState > 0) {
+      //counterState -= 1
+      setCounterState(counterState - 1)
+      setCurrentSong(playedGame.playlist[counterState]['youtube_url'])
+    } else {
+      setButton(true)
+    }
+  }
+ 
+  const buttonDecor = () => {
+    if (button === true) {
+      return <button onClick={nextSong}>Next Song</button>
+    } else {
+      return <button onClick={prevSong}>Prev Song</button>
+    }
+  }
   return (
     <div>
       <h1>Game played: {playedGame.name}</h1>
       <p>Song: {currentSong}</p>
-      <button onClick={nextSong}>Next Song</button>
+      {buttonDecor()}
     </div>
   )
 }
