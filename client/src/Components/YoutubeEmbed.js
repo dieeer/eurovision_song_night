514ae7e6d67c3259c7bb70dacd27e13163865511
Steven Todd <steven@flare-photography.com>
2022-09-25 16:09:30 +0100

    LATEST FROM STEVEN

---
+++ client/src/Components/YoutubeEmbed.js
@@ -0,0 +1,23 @@
+// import "../css/YoutubeEmbed.css"
+
+const YoutubeEmbed = ({url}) => {
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
+    let match = url.match(regExp)
+    let songYoutubeID = match && match[7].length == 11 ? match[7] : false
+
+return (
+  <div className='video-responsive'>
+    <iframe
+      width='427'
+      height='240'
+      src={`https://www.youtube.com/embed/${songYoutubeID}`}
+      frameBorder='0'
+      allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+      allowFullScreen
+      title='Embedded youtube'
+    />
+  </div>
+)
+}
+
+export default YoutubeEmbed
