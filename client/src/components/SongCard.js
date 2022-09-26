2947bbe789ecba63c5425bdecb6bcb4434698182
Steven Todd <steven@flare-photography.com>
2022-09-26 13:21:30 +0100

    Push of Steven's Songs code

--- client/src/components/SongCard.js
+++ client/src/components/SongCard.js
@@ -1,28 +1,6 @@
 import YoutubeEmbed from "./YoutubeEmbed";
-import { useState, useEffect } from "react";
-import playlistService from "../services/playlistService";
 
 const SongCard = ({song}) => {
-
-
-    // const [playlist, setPlaylist] = useState(playlists)
-
-    // // Add a song to a playlist.
-    // const addSongToPlaylist = (playlists) => {
-    //     const tempPlaylist = playlists[0]
-    //     tempPlaylist.songs.push(song)
-    //     playlistService.updatePlaylist(tempPlaylist)
-    // }
-
-
-    // // playlist options
-    // const playlistOptions = playlist.map((playlist) => {
-    //     return (
-    //         <option key={playlist._id} value={playlist._id}>{playlist.name}</option>
-    //     )
-    // })
-
-
 
 const youTube = song.youtube_url;
   return (
@@ -34,14 +12,6 @@ const youTube = song.youtube_url;
         <p>Composers: {song.composers}</p>
         <p>Country: {song.to_country}</p>
         <p>Year: {song.year}</p>
-        {/* <form onSubmit={addSongToPlaylist} playlists={playlists}>
-        <select name='playlist'>
-        {playlistOptions}
-        </select>
-        <input type='submit' name='submit'  value='Add to Playlist' /> */}
-
-        {/* </form> */}
-
     </div>
     </>
 )};
