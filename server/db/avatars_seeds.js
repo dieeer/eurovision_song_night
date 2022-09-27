b14e18a5d77f0f7b793f81388d4f850057199f69
Gregor Campbell <gregorc93@hotmail.co.uk>
2022-09-27 15:38:18 +0100

    Gregor, removed age from the database

--- server/db/avatars_seeds.js
+++
@@ -1,7 +0,0 @@
-use eurovision;
-
-db.avatars.insertMany([
-  {
-    image:'https://cdn.game.net/image/upload/b_rgb:FFFFFF,c_pad,dpr_auto,f_auto,h_714,q_auto,w_1000/c_pad,h_714,w_1000/v1/game_img/ml2/8/1/4/3/814350_gen_a?pgw=1&pgwact=1'
-}
-])
--- server/db/players_seeds.js
+++ server/db/players_seeds.js
@@ -1,37 +1,32 @@
 use eurovision;
-db.dropDatabase();
+
 db.players.insertMany([
   {
   name: "Michael",
-  age: "29",
-  sex:  "Male",
-  img: "https://e7.pngegg.com/pngimages/225/208/png-clipart-chin-man-glasses-emotion-beard-man-face-people.png"
+ 
+  img: "https://e7.pngegg.com/pngimages/225/208/png-clipart-chin-man-glasses-emotion-beard-man-face-people.png",
   
   
 },
 {
     name: "Stingey Stevens",
-    age: "24",
-    sex:  "Male",
+  
     img: "https://e7.pngegg.com/pngimages/733/440/png-clipart-man-holding-his-head-psychology-of-anger-controlling-anger-anger-management-feeling-angry-child-face.png"
 },
 {
     name: "Emma",
-    age: "31",
-    sex:  "Female",
+   
     img: 'https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png'
     
 },
 {
     name: "Brandon Legend",
-    age: "34",
-    sex:  "Male",
+   
     img: "https://e7.pngegg.com/pngimages/733/440/png-clipart-man-holding-his-head-psychology-of-anger-controlling-anger-anger-management-feeling-angry-child-face.png"
 },
 {
     name: "Vicky",
-    age: "40",
-    sex:  "Female",
+  
     img: "https://e7.pngegg.com/pngimages/225/208/png-clipart-chin-man-glasses-emotion-beard-man-face-people.png"
 }
 ])
